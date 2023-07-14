7d54d6bc14429bfde08cf138ba6bcf8e3525775a
mromanowska974 <m.romanowska974@gmail.com>
2023-07-14 08:33:15 +0100

    Got data of Business Service for Edit Business Service component from db
    Not working fully yet (tables and objects values)

--- src/main/webapp/app/entities/business-service/edit/business-service-edit.component.ts
+++ src/main/webapp/app/entities/business-service/edit/business-service-edit.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IBusinessService } from '../business-service.model';
+import { BusinessServiceService } from '../service/business-service.service';
 
 @Component({
   selector: 'jhi-business-service-edit',
   templateUrl: './business-service-edit.component.html',
   styleUrls: ['./business-service-edit.component.scss'],
 })
-export class BusinessServiceEditComponent {
+export class BusinessServiceEditComponent implements OnInit {
   sectionSelected: string = 'A';
 
-  constructor(private router: Router) {}
+  businessServiceId: number;
+  businessService: IBusinessService | null;
+  isDataLoaded: boolean = false;
+
+  constructor(private router: Router, private route: ActivatedRoute, private businessServiceService: BusinessServiceService) {}
+
+  ngOnInit(): void {
+    this.businessServiceId = +this.route.snapshot.params['id'];
+    this.getBusinessService();
+  }
+
+  getBusinessService() {
+    this.businessServiceService.find(this.businessServiceId).subscribe(businessService => {
+      this.businessService = businessService.body;
+      console.log(this.businessService);
+      this.isDataLoaded = true;
+    });
+  }
 
   onSelectSection(section: string) {
     this.sectionSelected = section;
